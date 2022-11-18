bf0dbd30ad5e76e9c64a114cd6ff6aee3f42a7c4
jjense8 <jjense8@uwo.ca>
2022-11-18 09:27:58 -0500

    database setup

---
+++ database-setup /setup.sql
@@ -0,0 +1,15 @@
+/* Enter local database in Terminal  */
+
+/usr/local/mysql/bin/mysql -u root -p
+
+/* Enter the password for your local database */
+
+--------------------------------------------------
+
+/* Display all databases */
+
+show databases; 
+
+
+/* Create database */
+CREATE DATABASE Lab3;
